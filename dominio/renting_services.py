5bb5aecf5bba67ae345b9c1ca45eb94c5048c9d3
Yuri3358 <yurialmeida917@gmail.com>
2026-01-02 09:51:32 -0300

    fix: doesn't selecting the tuple elements

--- dominio/renting_services.py
+++ dominio/renting_services.py
@@ -7,7 +7,7 @@ class RentingService:
         self.renting_repo = renting_repo
         self.vehicle_repo = vehicle_repo
 
-    def create_renting(self, renting_id: str, license_plate: str, client_id: str, start: date, end: date):
+    def create_rentings(self, renting_id: str, license_plate: str, client_id: str, start: date, end: date):
         vehicle_existence = self.vehicle_repo.get_vehicles_by_license(license_plate)
 
         if not vehicle_existence:
@@ -15,8 +15,17 @@ class RentingService:
 
         renting = Renting(renting_id, license_plate, client_id, start, end)
 
-        current_rentings = self.renting_repo.get_vehicle_by_license_plate(license_plate)
-
+        raw_rentings = self.renting_repo.get_vehicle_by_license_plate(license_plate)
+        current_rentings = [
+            Renting(
+                renting_id=r[0],
+                license_plate=r[1],
+                client_id=r[2],
+                start=r[3],
+                end=r[4],
+            )
+            for r in raw_rentings
+        ]
         RentingRules.check_renting_conflicts(renting, current_rentings)
 
         RentingRules.check_renting_period(renting, min_days=1, max_days=30)
